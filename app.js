fc5a81f2e3ba5249e3114e59122f261cf3d020d7
ABC-OPQ-XYZ <vine15branch@gmail.com>
2026-07-24 16:47:52 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+const hideoverlay = document.getElementById('hideoverlay');
+const overlay = document.getElementById('overlay')
+hideoverlay.addEventListener('click', () => {
+    overlay.style.display = 'none';
+})
